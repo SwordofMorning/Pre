3b2900017d5955b6e2297f6fdccd1577bb68b670
xjt <969211735@qq.com>
2024-11-15 12:19:42 +0800

    log

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
+#include "../others/version/version.h"
+#include "./utils/log/litelog.h"
 #include "./media/vi/v4l2_dvp.h"
+
+void init_modules()
+{
+    /* Init Modules */
+    litelog.init("Pre");
+
+    litelog.log.notice("Start!");
+    litelog.log.notice("Branch: %s", __GIT_BRANCH__);
+    litelog.log.notice("Commit ID: %s", __GIT_COMMIT_ID__);
+    litelog.log.notice("User: %s", __GIT_USER__);
+}
+
+void exit_modules()
+{
+    litelog.log.notice("End!");
+    litelog.exit();
+}
+
+void execute()
+{
+    DVP_Streaming();
+}
 
 int main()
 {
-    std::cout << "hello world" << std::endl;
+    init_modules();
 
-    DVP_Streaming();
+    execute();
+
+    exit_modules();
 
     return 0;
 }
