f6a02782fcd2a52afc3afb8b86166097aef3568c
xjt <969211735@qq.com>
2024-11-25 14:50:42 +0800

    Feat: White n Black hot
    1. add white black hot and white hot;
    2. feat std::alogrithm in Pseudo();
    3. add .bin in `others/pseudo/`.

--- src/media/algo/algo.cpp
+++ src/media/algo/algo.cpp
@@ -22,7 +22,7 @@ int Process_One_Frame()
         }
     }
 
-    // float输出
+    // float
     float scale = 1.0f / (max_val - min_val);
     for (int i = 0; i < v4l2_ir_dvp_valid_height; i++)
     {
@@ -33,66 +33,107 @@ int Process_One_Frame()
         }
     }
 
-    // YUV420P输出（使用伪彩色）
+    // YUV420P
     Pseudo(algo_in, y, u, v, v4l2_ir_dvp_valid_width, v4l2_ir_dvp_valid_height);
 
     return 0;
 }
 
+#include <algorithm>
+#include <cstring>
+
 void Pseudo(uint16_t* input, uint8_t* y_out, uint8_t* u_out, uint8_t* v_out, int width, int height)
 {
-    // 找出数据范围
-    uint16_t min_val = 65535;
-    uint16_t max_val = 0;
+    auto [min_it, max_it] = std::minmax_element(input, input + width * height);
+    uint16_t min_val = *min_it;
+    uint16_t max_val = *max_it;
 
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            uint16_t val = input[i * width + j];
-            if (val < min_val)
-                min_val = val;
-            if (val > max_val)
-                max_val = val;
-        }
-    }
-
-    // 避免除零错误
     if (max_val == min_val)
         max_val = min_val + 1;
 
-    if (usr.pseudo != PSEUDO_BLACK_HOT or usr.pseudo != PSEUDO_WHITE_HOT)
+    float scale = 255.0f / (max_val - min_val);
+
+    // clang-format off
+    switch(usr.pseudo)
     {
-        const struct YUV420P_LUT* lut = Get_LUT(usr.pseudo);
-        // Y分量映射
-        for (int i = 0; i < height; i++)
+        case PSEUDO_BLACK_HOT:
         {
-            for (int j = 0; j < width; j++)
+            // Y
+            for (int i = 0; i < height; i++)
             {
-                uint16_t val = input[i * width + j];
-                // 将值映射到颜色表范围
-                int color_idx = ((uint32_t)(val - min_val) * (lut->size - 1)) / (max_val - min_val);
-                y_out[i * width + j] = lut->y[color_idx];
+                for (int j = 0; j < width; j++)
+                {
+                    uint16_t val = input[i * width + j];
+                    y_out[i * width + j] = 255 - (uint8_t)((val - min_val) * scale);
+                }
             }
+
+            // UV
+            size_t uv_size = (width * height) / 4;
+            std::memset(u_out, 128, uv_size);
+            std::memset(v_out, 128, uv_size);
+            break;
         }
 
-        // UV分量需要4:2:0采样
-        for (int i = 0; i < height / 2; i++)
+        case PSEUDO_WHITE_HOT:
         {
-            for (int j = 0; j < width / 2; j++)
+            // Y
+            for (int i = 0; i < height; i++)
             {
-                uint16_t val00 = input[(i * 2) * width + (j * 2)];
-                uint16_t val01 = input[(i * 2) * width + (j * 2 + 1)];
-                uint16_t val10 = input[(i * 2 + 1) * width + (j * 2)];
-                uint16_t val11 = input[(i * 2 + 1) * width + (j * 2 + 1)];
-
-                uint32_t avg_val = (val00 + val01 + val10 + val11) / 4;
-
-                int color_idx = ((uint32_t)(avg_val - min_val) * (lut->size - 1)) / (max_val - min_val);
-
-                u_out[i * (width / 2) + j] = lut->u[color_idx];
-                v_out[i * (width / 2) + j] = lut->v[color_idx];
+                for (int j = 0; j < width; j++)
+                {
+                    uint16_t val = input[i * width + j];
+                    y_out[i * width + j] = (uint8_t)((val - min_val) * scale);
+                }
             }
+
+            // UV
+            size_t uv_size = (width * height) / 4;
+            std::memset(u_out, 128, uv_size);
+            std::memset(v_out, 128, uv_size);
+            break;
+        }
+
+        default:
+        {
+            const struct YUV420P_LUT* lut = Get_LUT(usr.pseudo);
+            if (!lut)
+                break;
+
+            // Y
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    uint16_t val = input[i * width + j];
+                    int color_idx = ((uint32_t)(val - min_val) * (lut->size - 1)) / (max_val - min_val);
+                    y_out[i * width + j] = lut->y[color_idx];
+                }
+            }
+
+            // UV, 420P
+            for (int i = 0; i < height/2; i++)
+            {
+                for (int j = 0; j < width/2; j++)
+                {
+                    // mean of 2x2
+                    uint32_t sum = 0;
+                    for(int di = 0; di < 2; di++)
+                    {
+                        for(int dj = 0; dj < 2; dj++)
+                        {
+                            sum += input[(i*2+di) * width + (j*2+dj)];
+                        }
+                    }
+                    uint32_t avg_val = sum / 4;
+                    
+                    int color_idx = ((uint32_t)(avg_val - min_val) * (lut->size - 1)) / (max_val - min_val);
+                    u_out[i * (width/2) + j] = lut->u[color_idx];
+                    v_out[i * (width/2) + j] = lut->v[color_idx];
+                }
+            }
+            break;
         }
     }
+    // clang-format on
 }
