d8baf0d273ecf85130bdf8535b860bc5835e40b2
xjt <969211735@qq.com>
2024-12-19 16:14:12 +0800

    uart class

--- src/utils/uart/motor.h
+++ src/utils/uart/motor.h
@@ -1,43 +1,11 @@
 #pragma once
 
-#include <string>
-#include <thread>
-#include <atomic>
-#include <functional>
-#include <unistd.h>
-#include <iostream>
-#include "uartwrap.h"
+#include "uartclass.h"
 
-class Motor
+class Motor : public UART
 {
 private:
-    static constexpr const char* DEVICE_NAME = "/dev/ttyS6";
-    static constexpr int BAUDRATE = 115200;
-    static constexpr int BUFFER_SIZE = 1024;
-
-    int m_fd;
-    std::thread m_listen_thread;
-    std::atomic<bool> m_running; // 运行状态标志
-
-private:
-    // 监听线程函数
-    void ListenThread();
-
-    // 回调函数，用于处理接收到的数据
-    std::function<void(const uint8_t*, size_t)> m_receive_callback;
-
-    std::mutex m_write_mutex;
 
 public:
     Motor();
-    ~Motor();
-
-    bool Start();
-    void Stop();
-
-    void operator()();
-
-    void SetCallback(std::function<void(const uint8_t*, size_t)> callback);
-
-    bool Send(const std::vector<uint8_t>& data);
 };
